280baa7de83f44b91c6954e39f038123b6428790
John Peters <3665510+CieloVistaSoftware@users.noreply.github.com>
2026-08-13 22:24:22 -0500

    docs: add 2026-08-14 What's New entry

--- src/core/version.js
+++ src/core/version.js
@@ -4,6 +4,6 @@
  */
 export const VERSION = {
   "version": "3.0.10",
-  "commit": "eba5a0d",
-  "builtAt": "2026-08-14T03:23:25.078Z"
+  "commit": "0cd67fd",
+  "builtAt": "2026-08-14T03:24:22.281Z"
 };
